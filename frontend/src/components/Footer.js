04828d763c4abc11e941769b9c183db711b64550
Subashji2512 <subashjipbk@gmail.com>
2025-05-13 15:32:22 +0530

    changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p>&copy; 2025 AI-Powered BFSI Doc Processor. All rights reserved.</p>
-      <p>Contact: support@bsfiapp.com | +91-99999-99999</p>
+       <p>Contact: subashjipbk@gmail.com | +91-7010770080</p>
     </footer>
   );
 }
